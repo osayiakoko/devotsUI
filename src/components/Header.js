d091488f600338e69935b03203e6fef939ce0b9d
Osayi Akoko <osayistreams@gmail.com>
2021-07-04 09:39:15 +0100

    fixed key & className error

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,20 +9,20 @@ const Header = () => {
     const navigation = ['HOME', 'CAST A VOTE', 'PROFILE', 'ABOUT']
 
     return (
-        <header class="relative h-96">
+        <header className="relative h-96">
             <img src={vote_ballot} alt="" 
-                class="w-full h-full filter grayscale object-cover"
+                className="w-full h-full filter grayscale object-cover"
             ></img>
-            <div id="overlay" class="absolute inset-0 bg-black opacity-70"></div>
-            <div class="absolute inset-0 text-white p-8">
-                <Popover as="nav" class="relative select-none">
+            <div id="overlay" className="absolute inset-0 bg-black opacity-70"></div>
+            <div className="absolute inset-0 text-white p-8">
+                <Popover as="nav" className="relative select-none">
                     {({open}) => (
-                        <div class="absolute w-full flex justify-between items-center" >
-                            <h2 class="text-2xl">Voter BALLOT</h2>
+                        <div className="absolute w-full flex justify-between items-center" >
+                            <h2 className="text-2xl">Voter BALLOT</h2>
 
-                            <ul class="hidden space-x-3 md:flex">
-                                { navigation.map(item => 
-                                    <li class="hover:text-blue-500 cursor-pointer">{item}</li>
+                            <ul className="hidden space-x-3 md:flex">
+                                { navigation.map((item, index) => 
+                                    <li key={index} className="hover:text-blue-500 cursor-pointer">{item}</li>
                                 )}
                             </ul>
 
@@ -45,8 +45,8 @@ const Header = () => {
                                     className="absolute top-10 w-full z-10 bg-white text-gray-800 rounded px-4 py-5 md:hidden"
                                 >
                                     <div className="grid">
-                                        { navigation.map(item => 
-                                            <a href="#" className="my-0.5 p-2 rounded hover:bg-gray-300">{item}</a>
+                                        { navigation.map((item, index) => 
+                                            <a href="#" key={index} className="my-0.5 p-2 rounded hover:bg-gray-300">{item}</a>
                                         )}
                                     </div>
                                 </Popover.Panel>
@@ -54,8 +54,8 @@ const Header = () => {
                         </div>
                     )}
                 </Popover>
-                <div class="h-full flex flex-col justify-center items-center text-center">
-                    <h2 class="pb-2 text-xl font-bold">WHY CHOOSE US?</h2>
+                <div className="h-full flex flex-col justify-center items-center text-center">
+                    <h2 className="pb-2 text-xl font-bold">WHY CHOOSE US?</h2>
                     <p>We take democracy seriously. Find out why our clients consistently rely on us for their needs</p>
                 </div>
             </div>
