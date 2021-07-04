1b8a3fe87c2a56b14ab23ec1dd90e1fd1615da18
Osayi Akoko <osayistreams@gmail.com>
2021-07-04 12:31:12 +0100

    footer absolute to bottom

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 
 const Footer = () => {
     return (
-        <footer className="text-center text-gray-800 p-6">
+        <footer className="absolute w-full top-full bg-gray-300 text-center text-gray-800 p-6">
             <span className="font-bold cursor-pointer hover:text-blue-700"> CONTACT </span> | <span className="font-bold cursor-pointer hover:text-blue-700"> PRIVACY </span> | <span className="font-bold hover:text-blue-700" href="#"> LEGAL </span>
         </footer>
     )
